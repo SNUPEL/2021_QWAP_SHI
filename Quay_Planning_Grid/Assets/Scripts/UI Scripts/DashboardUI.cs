17364148c2312ee4fb2cedb7c94d37848bb95ce3
WonSeok Choi <cwss0154@gmail.com>
2025-09-03 17:26:10 +0900

    Unity project: [Windows ONLY] Add OpenFileDialog

--- Quay_Planning_Grid/Assets/Scripts/UI Scripts/DashboardUI.cs
+++ Quay_Planning_Grid/Assets/Scripts/UI Scripts/DashboardUI.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.IO;
+
+#if UNITY_EDITOR_WIN
+using System.Windows.Forms;
+#endif
 
 public class DashboardUI : MonoBehaviour
 {
     //public ScheduleManager scheduleManager;
     //public SPT_ScheduleManager SPTScheduleManager;
     public TMP_Text algorithmNameText;
+    public TextMeshProUGUI textFilePath;
+    public TextMeshProUGUI textFileName;
 
     [Header("Cameras")]
     public Camera cam1;
@@ -88,5 +95,36 @@ public class DashboardUI : MonoBehaviour
     {
         if (algorithmNameText != null)
             algorithmNameText.text = $"{name} Algorithm";
+    }
+
+    public void OnTypeBClicked()
+    {
+        SimulationController.Instance?.ResetSimulation();
+#if UNITY_EDITOR_WIN
+        using (OpenFileDialog ofd = new OpenFileDialog())
+        {
+            ofd.Filter = "All Files (*.*)|*.*";
+            ofd.Title = "Select a file";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                // = ofd.FileName;  // 선택한 파일 경로 반환
+                                                   // 파일명 추출
+                string fileName = Path.GetFileName(ofd.FileName);
+
+                // 상위 디렉토리 2개 추출
+                DirectoryInfo dir = new DirectoryInfo(Path.GetDirectoryName(ofd.FileName));
+                string dir1 = dir?.Name;               // 현재 디렉토리
+                string dir2 = dir?.Parent?.Name;       // 상위 디렉토리
+
+                if (!string.IsNullOrEmpty(dir2))
+                    textFilePath.text = $"{dir2}/{dir1}/{fileName}";
+                if (!string.IsNullOrEmpty(dir1))
+                    textFilePath.text = $"{dir1}/{fileName}";
+                textFileName.text = Path.GetFileNameWithoutExtension(ofd.FileName);
+            }
+        }
+#endif
+
     }
 }
